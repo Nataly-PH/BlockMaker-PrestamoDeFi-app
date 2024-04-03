58ac6da37f5d97e4d418c9fefc665b4d4febf3e7
nataly <15marzonataly@gmail.com>
2024-04-04 00:04:30 +0200

    Añadir App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import  { AppLayout } from './components/ui/layauts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { InicioYAutenticacion }  from './pages/Pagina_1';
+import { GestionDeUsuarios } from './pages/Pagina_2';
+import { OperacionesDePrestamos } from './pages/Pagina_3';
+import { Home } from './pages/Pagina_1';
+
 
 
 function App() {
-
-
   return (
-  
-      <h1 className="text-3xl font-bold underline bg-red-200">Hello world!</h1>
     
+        <Router>
+  
+          <AppLayout>
+            
+            <Routes>
+              <Route path="/" element={<InicioYAutenticacion></InicioYAutenticacion>}></Route>
+            </Routes>
+            <Routes>
+              <Route path="/usuarios" element={<GestionDeUsuarios></GestionDeUsuarios>}></Route>
+            </Routes>
+            <Routes>
+              <Route path="/prestamos" element={<OperacionesDePrestamos></OperacionesDePrestamos>}></Route>
+            </Routes>
+
+            <Home></Home>
+
+
+          </AppLayout>
+
+        </Router>
+    
+
+    
+   
   )
 }
 
-export default App
+export default App;
