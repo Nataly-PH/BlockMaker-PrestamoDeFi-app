1dbd5ca9f1abc0b77e4b6a3e7392ded321ea09d3
nataly <15marzonataly@gmail.com>
2024-04-03 23:25:41 +0200

    Añadir componente Footer

---
+++ src/components/CustomFooter.jsx
@@ -0,0 +1,6 @@
+export default function CustomFooter() {
+    return <footer className="border-t py-4 px-3 flex justify-center bg-purple-200 shadow-xs font-bold">
+        <p>Derechos de autor &copy; {new Date().getFullYear()} Blockmaker Academy</p>
+
+    </footer>;
+} 
